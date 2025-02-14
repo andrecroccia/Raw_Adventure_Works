812e2a4f7a9f947708a92330bab555ec5598f5ea
André Luiz Croccia Motta da Silva da Silva <andrecroccia93@gmail.com>
2025-02-14 14:14:07 +0000

    finalizado as duas tabelas fatos (pedidos e pedidos detalhe)

---
+++ models/intermediate/int_erp__pedido_detalhe_por_produto.sql
@@ -0,0 +1,22 @@
+WITH
+produtos AS ( SELECT * FROM {{ ref('int_erp__produtos') }} ),
+
+pedidos_detalhe AS ( SELECT * FROM {{ ref('stg_erp__vendas_por_pedido_detalhe') }} ),   -- join produtos em pedidos detalhe
+
+pedido_detalhe_por_produto AS (
+    SELECT
+   
+       ID_PEDIDO
+        , pd.ID_PEDIDO_DETALHE
+        , pd.QTDE
+        , p.nome_do_produto as produto
+        , pd.PRECO_UNITARIO
+        , pd.PRECO_UNITARIO_DESCONTO
+        , pd.VALOR_TOTAL
+
+    FROM pedidos_detalhe pd
+    left JOIN produtos p ON pd.id_produto = p.produto_id )
+    
+
+select *
+from pedido_detalhe_por_produto
---
+++ models/intermediate/int_erp__pedidos.sql
@@ -0,0 +1,48 @@
+WITH
+pedidos AS ( SELECT * FROM {{ ref('stg_erp__vendas_por_pedido') }} ),
+
+endereco AS ( SELECT * FROM {{ ref('dim_endereco') }} ),   
+
+cliente AS ( SELECT * FROM {{ ref('dim_clientes') }} ),
+
+motivo_venda AS ( SELECT * FROM {{ ref('dim_motivo_venda') }} ),
+
+cartao_de_credito AS ( SELECT * FROM {{ ref('dim_cartao_de_credito') }} ),
+
+
+joined AS (
+    SELECT
+
+         p.ID_PEDIDO
+        , p.DATA_DE_VENDA
+       -- , p.DATA_DE_VENCIMENTO
+       -- , p.DATA_DE_ENVIO
+        , p.STATUS
+       -- , p.NUMERO_DA_CONTA
+       -- , p.ID_CLIENTE
+        , c.NOME_CLIENTE
+        , p.ID_VENDEDOR
+       -- , p.ID_TERRITORIO
+        , e.CIDADE
+        , e.NOME_DO_ESTADO AS ESTADO
+        , e.NOME_PAIS AS PAIS
+       -- , p.ID_METODO_ENVIO
+       -- , p.ID_CARTAO_TIPO
+        , cc.tipo_cartao_de_credito as cartao_de_credito
+        , p.SUBTOTAL
+        , p.TAXAMT
+        , p.FRETE
+        , p.VALOR_TOTAL
+        , m.MOTIVO_DE_VENDA
+
+    FROM pedidos p
+    left JOIN endereco e on p.id_endereco = e.endereco_id 
+    left join cliente c on p.id_cliente = c.id_cliente
+    left join motivo_venda m on p.id_pedido = m.id_pedido
+    left join cartao_de_credito cc on p.id_cartao_tipo = cc.cartao_de_credito_id
+
+    )
+    
+
+select *
+from joined
