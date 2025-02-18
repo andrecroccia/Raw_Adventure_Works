13849626d2f381e46274a16e5c070a84af1c726f
André Luiz Croccia Motta da Silva da Silva <andrecroccia93@gmail.com>
2025-02-18 14:26:48 +0000

    primeira alteração em realação a nomes duplicados

--- models/stg/stg_erp__pessoa.sql
+++ models/stg/stg_erp__pessoa.sql
@@ -1,18 +1,44 @@
-with
-fonte_pessoa as (
-select *   
-from {{ source('erp', 'person') }}
+ WITH fonte_pessoa AS (
+     SELECT *   
+     FROM {{ source('erp', 'person') }}
+ ),
+ renomeado AS (
+     SELECT
+         CAST(BUSINESSENTITYID AS INT) AS id_pessoa
+         , CAST(PERSONTYPE AS STRING) AS pessoa_tipo
+         , COALESCE(FIRSTNAME, '') || ' ' || 
+          COALESCE(MIDDLENAME, '') || ' ' || 
+          COALESCE(LASTNAME, '') AS nome_cliente
+     FROM fonte_pessoa
+ )
+ 
+ SELECT *
+ FROM renomeado
 
-)
-, renomeado as (
-    select
-        cast(BUSINESSENTITYID as int) as id_pessoa
-        ,cast(PERSONTYPE as string) as pessoa_tipo
-        ,FIRSTNAME || ' ' || LASTNAME as nome_cliente
-        
+-- WITH fonte_pessoa AS ( -- VERIFICAR QUAIS NOMES ESTÃO DUPLICADOS
+--     SELECT *   
+--     FROM {{ source('erp', 'person') }}
+-- ),
+-- renomeado AS (
+--     SELECT
+--         CAST(BUSINESSENTITYID AS INT) AS id_pessoa,
+--         CAST(PERSONTYPE AS STRING) AS pessoa_tipo,
+--         COALESCE(FIRSTNAME, '') || ' ' || 
+--         COALESCE(MIDDLENAME, '') || ' ' || 
+--         COALESCE(LASTNAME, '') AS nome_cliente
+--     FROM fonte_pessoa
+-- )
+-- , duplicados AS (
+--     SELECT 
+--         nome_cliente, 
+--         COUNT(*) AS qtd_ocorrencias
+--     FROM renomeado
+--     GROUP BY nome_cliente
+--     HAVING COUNT(*) > 1
+-- )
+-- 
+-- SELECT * 
+-- FROM duplicados
+-- ORDER BY qtd_ocorrencias DESC
 
-    from fonte_pessoa
-)
 
-select *
-from renomeado
